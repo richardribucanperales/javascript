05e4c917d014cb1259c40e6a6a65377109c1dcdd
gawigwen <19rodnce@waipahuh.k12.hi.us>
2017-11-08 13:20:06 -1000

    product is complete

--- conversions.js
+++ conversions.js
@@ -1,1 +1,130 @@
-var c = (f32)/1.8
+/*(f32)/1.8*/
+var a = 20;
+document.getElementById("a").innerHTML = a
+
+var ab = a + 5;
+document.getElementById("ab").innerHTML = ab
+
+var ac = ab + 5;
+document.getElementById("ac").innerHTML = ac
+
+var ad = ac + 5;
+document.getElementById("ad").innerHTML = ad
+
+var ae = ad + 5;
+document.getElementById("ae").innerHTML = ae
+
+var af = ae + 5;
+document.getElementById("af").innerHTML = af
+
+var ag = af + 5;
+document.getElementById("ag").innerHTML = ag
+
+var ah = ag + 5;
+document.getElementById("ah").innerHTML = ah
+
+var ai = ah + 5;
+document.getElementById("ai").innerHTML = ai
+
+var aj = ai + 5;
+document.getElementById("aj").innerHTML = aj
+
+var ak = aj + 5;
+document.getElementById("ak").innerHTML = ak
+
+var al = ak + 5;
+document.getElementById("al").innerHTML = al
+
+var am = al + 5;
+document.getElementById("am").innerHTML = am
+
+var an = am + 5;
+document.getElementById("an").innerHTML = an
+
+var ao = an + 5;
+document.getElementById("ao").innerHTML = ao
+
+var ap = ao + 5;
+document.getElementById("ap").innerHTML = ap
+
+var aq = ap + 5;
+document.getElementById("aq").innerHTML = aq
+
+var ar = aq + 5;
+document.getElementById("ar").innerHTML = ar
+
+var az = ar + 5;
+document.getElementById("az").innerHTML = az
+
+var at = az + 5;
+document.getElementById("at").innerHTML = at
+
+var au = at + 5;
+document.getElementById("au").innerHTML = au
+
+var mul = 32
+
+var div = 1.8
+
+var b = (a - mul)/div;
+document.getElementById("b").innerHTML = b
+
+var ba = (ab - mul)/div;
+document.getElementById("ba").innerHTML = ba
+
+var bb = (ac - mul)/div;
+document.getElementById("bb").innerHTML = bb
+
+var bc = (ad - mul)/div;
+document.getElementById("bc").innerHTML = bc
+
+var bd = (ae - mul)/div;
+document.getElementById("bd").innerHTML = bd
+
+var be = (af - mul)/div;
+document.getElementById("be").innerHTML = be
+
+var bf = (ag - mul)/div;
+document.getElementById("bf").innerHTML = bf
+
+var bg = (ah - mul)/div;
+document.getElementById("bg").innerHTML = bg
+
+var bh = (ai - mul)/div;
+document.getElementById("bh").innerHTML = bh
+
+var bi = (aj - mul)/div;
+document.getElementById("bi").innerHTML = bi
+
+var bj = (ak - mul)/div;
+document.getElementById("bj").innerHTML = bj
+
+var bk = (al - mul)/div;
+document.getElementById("bk").innerHTML = bk
+
+var bl = (am - mul)/div;
+document.getElementById("bl").innerHTML = bl
+
+var bm = (an - mul)/div;
+document.getElementById("bm").innerHTML = bm
+
+var bn = (ao - mul)/div;
+document.getElementById("bn").innerHTML = bn
+
+var bo = (ap - mul)/div;
+document.getElementById("bo").innerHTML = bo
+
+var bp = (aq - mul)/div;
+document.getElementById("bp").innerHTML = bp
+
+var bq = (ar - mul)/div;
+document.getElementById("bq").innerHTML = bq
+
+var br = (az - mul)/div;
+document.getElementById("br").innerHTML = br
+
+var bs = (at - mul)/div;
+document.getElementById("bs").innerHTML = bs
+
+var bt = (au - mul)/div;
+document.getElementById("bt").innerHTML = bt
